150c4eb89ce025baa7db8c122d9650383fe914e4
alice whaleshark <126924511+wh4l35h4rk@users.noreply.github.com>
2025-02-05 04:02:33 +1000

    feat: json to http responses

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -10,30 +10,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/project')]
 final class ProjectController extends AbstractController
 {
     #[Route(name: 'app_project_index', methods: ['GET'])]
-    public function index(ProjectRepository $projectRepository, SerializerInterface $serializer): JsonResponse
+    public function index(ProjectRepository $projectRepository): Response
     {
         $projects = $projectRepository->findAll();
 
-        $this->render('project/index.html.twig', [
+        foreach ($projects as $project) {
+            if ($project->getProjectGroup()) {
+                $project->getProjectGroup()->getName();
+            }
+        }
+
+        return $this->render('project/index.html.twig', [
             'projects' => $projects,
         ]);
-
-        $jsonData = $serializer->serialize($projects, 'json', ['groups' => 'project:read']);
-
-        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
 
     #[Route('/new', name: 'app_project_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $project = new Project();
+        $project->setCreatedAt(new \DateTime());
+        $project->setUpdatedAt(new \DateTime());
+
         $form = $this->createForm(ProjectType::class, $project);
         $form->handleRequest($request);
 
@@ -41,71 +44,56 @@ final class ProjectController extends AbstractController
             $entityManager->persist($project);
             $entityManager->flush();
 
-            $jsonData = $serializer->serialize($project, 'json', ['groups' => 'project:read']);
-
-            return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+            return $this->redirectToRoute('app_project_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        $this->render('project/new.html.twig', [
+        return $this->render('project/new.html.twig', [
             'project' => $project,
             'form' => $form,
         ]);
-
-        $errors = [];
-        foreach ($form->getErrors(true) as $error) {
-            $errors[$error->getOrigin()->getName()][] = $error->getMessage();
-        }
-
-        return new JsonResponse(['data' => $errors]);
     }
 
     #[Route('/{id}', name: 'app_project_show', methods: ['GET'])]
-    public function show(Project $project, SerializerInterface $serializer): JsonResponse
+    public function show(Project $project): Response
     {
-        $this->render('project/show.html.twig', [
+        if ($project->getProjectGroup()) {
+            $project->getProjectGroup()->getName();
+        }
+
+        return $this->render('project/show.html.twig', [
             'project' => $project,
         ]);
-
-        $jsonData = $serializer->serialize($project, 'json', ['groups' => 'project:read']);
-    
-        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
 
     #[Route('/{id}/edit', name: 'app_project_edit', methods: ['GET', 'POST', 'PATCH'])]
-    public function edit(Request $request, Project $project, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function edit(Request $request, Project $project, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProjectType::class, $project);
         $form->handleRequest($request);
-    
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($request->getMethod() === 'PATCH') {
-                $entityManager->flush();
-                $jsonData = $serializer->serialize($project, 'json', ['groups' => 'project:read']);
 
-                return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+        if ($form->isSubmitted() && $request->isMethod('PATCH')) {
+            $project->setUpdatedAt(new \DateTime());
+
+            if ($form->isValid()) {
+                $entityManager->flush();
+                return $this->redirectToRoute('app_project_index', [], Response::HTTP_SEE_OTHER);
             }
         }
 
-        $errors = [];
-        foreach ($form->getErrors(true) as $error) {
-            $errors[$error->getOrigin()->getName()][] = $error->getMessage();
-        }
-    
-        return new JsonResponse(['data' => $errors]);
+        return $this->render('project/edit.html.twig', [
+            'project' => $project,
+            'form' => $form,
+        ]);
     }
-    
-    
 
     #[Route('/{id}', name: 'app_project_delete', methods: ['POST', 'DELETE'])]
-    public function delete(Request $request, Project $project, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(Request $request, Project $project, EntityManagerInterface $entityManager): Response
     {
-        if ($request->getMethod() == 'DELETE' && $this->isCsrfTokenValid('delete'.$project->getId(), $request->getPayload()->getString('_token'))) {
+        if ($request->isMethod('DELETE') && $this->isCsrfTokenValid('delete'.$project->getId(), $request->get('_token'))) {
             $entityManager->remove($project);
             $entityManager->flush();
-            
-            return new JsonResponse(['deleted successfully']);
         }
 
-        return new JsonResponse(['something went wrong!']);
+        return $this->redirectToRoute('app_project_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,31 +9,34 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/task')]
 final class TaskController extends AbstractController
 {
     #[Route(name: 'app_task_index', methods: ['GET'])]
-    public function index(TaskRepository $taskRepository, SerializerInterface $serializer): JsonResponse
+    public function index(TaskRepository $taskRepository): Response
     {
         $tasks = $taskRepository->findAll();
 
-        $this->render('project_groups/index.html.twig', [
-            'projects' => $tasks,
+        foreach ($tasks as $task) {
+            if ($task->getProject()) {
+                $task->getProject()->getName();
+            }
+        }
+
+        return $this->render('task/index.html.twig', [
+            'tasks' => $tasks,
         ]);
-
-        $jsonData = $serializer->serialize($tasks, 'json', ['groups' => 'task:read']);
-
-        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
     }
 
     #[Route('/new', name: 'app_task_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $task = new Task();
+        $task->setCreatedAt(new \DateTime());
+        $task->setUpdatedAt(new \DateTime());
+
         $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
 
@@ -41,64 +44,56 @@ final class TaskController extends AbstractController
             $entityManager->persist($task);
             $entityManager->flush();
 
-            $jsonData = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
-
-            return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+            return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        $this->render('project/show.html.twig', [
+        return $this->render('task/new.html.twig', [
             'task' => $task,
             'form' => $form,
         ]);
-
-        $errors = [];
-        foreach ($form->getErrors(true) as $error) {
-            $errors[$error->getOrigin()->getName()][] = $error->getMessage();
-        }
-
-        return new JsonResponse(['errors' => $errors]);
     }
 
     #[Route('/{id}', name: 'app_task_show', methods: ['GET'])]
-    public function show(Task $task, SerializerInterface $serializer): JsonResponse
+    public function show(Task $task): Response
     {
-        $jsonData = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
+        if ($task->getProject()) {
+            $task->getProject()->getName();
+        }
 
-        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+        return $this->render('task/show.html.twig', [
+            'task' => $task,
+        ]);
     }
 
     #[Route('/{id}/edit', name: 'app_task_edit', methods: ['GET', 'POST', 'PATCH'])]
-    public function edit(Request $request, Task $task, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    public function edit(Request $request, Task $task, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid() && $request->getMethod() === 'PATCH') {
-            $entityManager->flush();
+        if ($form->isSubmitted() && $request->getMethod() === 'PATCH'){
+            $task->setUpdatedAt(new \DateTime());
 
-            $jsonData = $serializer->serialize($task, 'json', ['groups' => 'task:read']);
+            if ($form->isValid()) {
+                $entityManager->flush();
 
-            return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
-        }
+                return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
+        }}
 
-        $errors = [];
-        foreach ($form->getErrors(true) as $error) {
-            $errors[$error->getOrigin()->getName()][] = $error->getMessage();
-        }
-
-        return new JsonResponse(['errors' => $errors]);
+        return $this->render('task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
     }
 
     #[Route('/{id}', name: 'app_task_delete', methods: ['POST', 'DELETE'])]
-    public function delete(Request $request, Task $task, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(Request $request, Task $task, EntityManagerInterface $entityManager): Response
     {
         if ($request->getMethod() == 'DELETE' && $this->isCsrfTokenValid('delete'.$task->getId(), $request->get('_token'))) {
             $entityManager->remove($task);
             $entityManager->flush();
-
-            return new JsonResponse(['deleted successfully']);
         }
 
-        return new JsonResponse(['something went wrong!']);
+        return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
     }
 }
